fdf82b78d0963b794f9b4e18eeb147cdd18beb1b
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-03-02 16:13:56 +0900

    update: "메뉴 삭제기능 추가 구현"
    [x] 총 메뉴 갯수를 count하여 상단에 보여준다.

--- src/js/index.js
+++ src/js/index.js
@@ -34,62 +34,25 @@
 // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 const $ = (selector) => document.querySelector(selector);
-const addMenuName = () => {
-  //입력값이 비어있을 경우
-  if ($("#espresso-menu-name").value === "") {
-    alert("값을 입력해주세요.");
-    return; //return을 해주면 다음 부분까지 실행되지 않고 종료된다.
-  }
-
-  //입력값이 있을 경우
-  const espressoMenuName = $("#espresso-menu-name").value;
-  const menuItemTemplate = (espressoMenuName) => {
-    return `
-      <li class="menu-list-item d-flex items-center py-2">
-      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-      >
-        수정
-      </button>
-      <button
-        type="button"
-        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-      >
-        삭제
-      </button>
-    </li>`;
-  };
-  // <!-- beforebegin -->
-  //<ul>
-  //<!-- afterbegin -->
-  //<li></li>
-  //<!-- beforeend -->
-  //</ul>
-  //<!-- afterend -->
-  // $("#espresso-menu-list").innerHTML = menuItemTemplate(espressoMenuName);
-  $("#espresso-menu-list").insertAdjacentHTML(
-    "beforeend",
-    menuItemTemplate(espressoMenuName)
-  );
-  // const 변수 = li 갯수를 카운팅해서 구해보기
-  const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-  $(".menu-count").innerText = `총 ${menuCount}개`;
-  $("#espresso-menu-name").value = "";
-};
 
 function App() {
   //TODO 메뉴 삭제
   // - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌(confirm) 모달창이 뜬다.
   // - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-  // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
+  // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 
   //이벤트 위임 : li태그들이 이벤트가 동작이 되어야하는데
   //해당 코드가 존재하지 않기 때문에 그 부모인 ul태그에 해당 이벤트를 위임한다.
   //prompt("사용자에게 전달할 메세지 작성 가능","기본 값")
   //Element.closest() : 부모 요소 단위로 출발하여 각 요소가 지정한 선택자에 가장 가깝게 조건에 만족한 부모요소가 반환되며, 조건에 만족한 요소가 없으면 null 값을 반환한다.
   //Element.remove() : 삭제기능을 하는 method
+
+  // const 변수 = li 갯수를 카운팅해서 구해보기
+  const UpdateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount}개`;
+  };
+
   $("#espresso-menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
       const $menuName = e.target.closest("li").querySelector(".menu-name");
@@ -103,6 +66,7 @@ function App() {
     if (e.target.classList.contains("menu-remove-button")) {
       if (confirm("정말 삭제하시겠습니까?")) {
         e.target.closest("li").remove();
+        UpdateMenuCount();
       }
     }
   });
@@ -110,6 +74,50 @@ function App() {
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
+
+  const addMenuName = () => {
+    //입력값이 비어있을 경우
+    if ($("#espresso-menu-name").value === "") {
+      alert("값을 입력해주세요.");
+      return; //return을 해주면 다음 부분까지 실행되지 않고 종료된다.
+    }
+
+    //입력값이 있을 경우
+    const espressoMenuName = $("#espresso-menu-name").value;
+    const menuItemTemplate = (espressoMenuName) => {
+      return `
+        <li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+    };
+    // <!-- beforebegin -->
+    //<ul>
+    //<!-- afterbegin -->
+    //<li></li>
+    //<!-- beforeend -->
+    //</ul>
+    //<!-- afterend -->
+    // $("#espresso-menu-list").innerHTML = menuItemTemplate(espressoMenuName);
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate(espressoMenuName)
+    );
+    // 메뉴count기능
+    UpdateMenuCount();
+    $("#espresso-menu-name").value = "";
+  };
 
   $("#espresso-menu-submit-button").addEventListener("click", () => {
     addMenuName();
